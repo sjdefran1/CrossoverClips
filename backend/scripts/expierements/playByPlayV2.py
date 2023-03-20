8e815111b6b37c15024cc30e47b89b66bcd3df08
Sam <sjdefran@iastate.edu>
2023-03-19 22:36:17 -0500

    branching back to main

--- backend/scripts/expierements/playByPlayV2.py
+++ backend/scripts/expierements/playByPlayV2.py
@@ -1,6 +1,6 @@
 import requests
 import json
-
+#from scripts.playByPlay.playbyplayToUrls import getActionNumberToURLs
 
 headers = {
     'Accept': '*/*',
@@ -17,14 +17,84 @@ headers = {
     'sec-ch-ua-platform': '"macOS"',
 }
 
-url="https://stats.nba.com/stats/playbyplayv2?EndPeriod=1&GameID=0021600001&StartPeriod=1"
+def getActionNumberToURLs(gameID: str, stat_type='FGM') -> dict:
+  params = {
+      'GameID': gameID, # not required,
+      'ContextMeasure': stat_type,
+      'Month': '0', # required //
+      'OpponentTeamID': '0', # required //
+      'Period': '0', # required //
+      'PlayerID': '0', # required nullable
+      #'RangeType': '0', # not required
+      #'Season': '2022-23', # not required
+      #'SeasonType': 'Regular Season', # not required
+      #'StartPeriod': '0', # not required
+      #'StartRange': '0', # not required
+      'TeamID': '0', # required //
+  }
+  try:
+    print(f"Making request to VideoDetail Asset ({stat_type})...")
+    response = requests.get('https://stats.nba.com/stats/videodetailsasset', params=params, headers=headers, timeout=15)
+    print("Request to VideoDetail Asset complete")
 
+  except:
+    print("request Timeout")
+  #print(response)
+  # with open("videos1.txt", "w") as f:
+  #   f.write(json.dumps(response.json(), indent=1))
+  # print(response.content)
+
+  urls = response.json()['resultSets']['Meta']['videoUrls']
+  #print(urls)
+  action_hex = {}
+
+  # parse uid for each action number
+  for element in urls:
+      if element['lurl'] != None:
+          # 'https://videos.nba.com/nba/pbp/media/2023/02/16/0022200885/9/e89c8c5a-78bb-4a1d-08af-b36491bffeda_1280x720.mp4'
+          # ['9/', 'e89c8c5a-78bb-4a1d-08af-b36491bffeda_1280x720.mp4']
+          # ['9', 'e89c8c5a-78bb-4a1d-08af-b36491bffeda_1280x720.mp4']
+          split = element['lurl'].split(f'{gameID}/')[1].split("/")
+          action_hex.update({split[0]: split[1]})
+  
+  #print(json.dumps(action_hex, indent=1))
+  #print(action_hex)
+  return action_hex
+
+# spurs mavs
+#url="https://stats.nba.com/stats/playbyplayv2?EndPeriod=100&GameID=0021400002&StartPeriod=1"
+
+#rockets pels
+#0022201067
+y = '2023'
+m = '03'
+d = '19'
+game_id = '0022201067'
+url=f"https://stats.nba.com/stats/playbyplayv2?EndPeriod=100&GameID={game_id}&StartPeriod=1"
 response = requests.get(url=url, headers=headers)
 
 response = response.json()
 
-print(json.dumps(response, indent=1))
+actions = response['resultSets'][0]['rowSet']
+actions_hex = getActionNumberToURLs(gameID=game_id, stat_type='BLK')
+#print(actions_hex)
+for action in actions:
+    #print(action[1])
+    action_num_str = f"{action[1]}"
+    if action_num_str not in actions_hex:
+        continue
+    vid_url = f"https://videos.nba.com/nba/pbp/media/{y}/{m}/{d}/{game_id}/{action_num_str}/{actions_hex[action_num_str]}"
+    desc = action[9] if action[7] is None else action[7]
+    
+    
+    print(f"{desc} {vid_url}")
+      
+#print(actions)
 
-with open("../../txt/playByPlayV2.txt", "w") as f:
-    f.write(json.dumps(response, indent=1))
+# print(json.dumps(response, indent=1))
+# f"https://videos.nba.com/nba/pbp/media/2014/10/28/{play['gi']}/{play['ei']}/" + action_hex.get(f"{play['ei']}")
+# with open("../../txt/playByPlayV2.txt_2", "w") as f:
+#     f.write(json.dumps(response, indent=1))
+
+#print(json.dumps(getActionNumberToURLs(gameID='0021400002'), indent=1))
 
