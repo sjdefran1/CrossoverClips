86fde709943cd79e917a75e9549ef99640244078
Sam DeFrancisco <72476187+sjdefran1@users.noreply.github.com>
2024-05-21 04:40:22 +0000

    Got rid of annoying resizing bug on video player

--- frontendV2/cc-ui/src/pages/players/playerVideoWrapper.jsx
+++ frontendV2/cc-ui/src/pages/players/playerVideoWrapper.jsx
@@ -1,9 +1,8 @@
 import { useState } from "react";
 import VideoFrame from "../../components/videoFrame";
-import { Paper, Stack, Box, IconButton, Hidden, Grid } from "@mui/material";
+import { Paper, Stack, IconButton, Hidden } from "@mui/material";
 import KeyboardArrowLeftIcon from "@mui/icons-material/KeyboardArrowLeft";
 import KeyboardArrowRightIcon from "@mui/icons-material/KeyboardArrowRight";
-import AspectRatioIcon from "@mui/icons-material/AspectRatio";
 import { useDispatch, useSelector } from "react-redux";
 import { incrementPlayIndex, setFullscreenVideo } from "./playerSlice";
 
@@ -26,7 +25,7 @@ export default function PlayerVideoWrapper() {
 
   return (
     <>
-      <Stack direction={"row"} alignItems={"center"} spacing={1}>
+      <Stack direction={"row"} alignItems={"center"}>
         <Hidden smDown>
           <IconButton onClick={handleLeftArrowClick}>
             <KeyboardArrowLeftIcon fontSize='large' color='info' />
@@ -42,7 +41,6 @@ export default function PlayerVideoWrapper() {
           setShowProgressBar={setShowProgressBar}
         />
 
-        {showProgressBar && <Box minHeight={"47.5vh"} width={"100%"}></Box>}
         <Hidden smDown>
           <IconButton onClick={handleRightArrowClick} sx={{ borderRadius: 5 }}>
             <KeyboardArrowRightIcon fontSize='large' color='info' />
