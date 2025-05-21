e80201d13c1f46ba548b833274f5dd89b6f99f38
Sam DeFrancisco <sjdefran@gmail.com>
2025-05-20 20:38:17 -0400

    remove first version of new workflows frm  this repo

--- cc-workflows/play-by-play/main.py
+++
@@ -1,6 +0,0 @@
-from playByPlayContext import Game, PlayByPlayContext
-
-if __name__ == "__main__":
-    game = Game(day=24, month=10, year=2023, gameId="0022300061")
-    context = PlayByPlayContext(game=game)
-    context.createPlayByPlay()
--- cc-workflows/wakeup/wakeup.py
+++
@@ -1,3 +0,0 @@
-import requests
-
-requests.get("https://nbaclipsite.onrender.com/")
